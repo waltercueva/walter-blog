e9c1b11140a76a16c5b3d01640c7b2c58b273618
Walter Cueva Chavez <waltercueva@gmail.com>
2020-11-01 21:34:00 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,13 @@ import sharedStyles from '../styles/shared.module.css'
 export default () => (
   <>
     <Header titlePre="Home" />
-    <div className={sharedStyles.layout}>
+    <!--<div className={sharedStyles.layout}>
       <img
         src="/vercel-and-notion.png"
         height="85"
         width="250"
         alt="Vercel + Notion"
-      />
+      />-->
       <h1>Blog Personal</h1>
       <h2>
         ...
